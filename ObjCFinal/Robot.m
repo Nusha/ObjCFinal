88154b66c3d0834d58c91e384709145fd8e42791
Ivan <i.balabai@gmail.com>
2023-12-07 22:08:54 +0300

    Robot class  save to file methods have been added;

--- ObjCFinal/Robot.m
+++ ObjCFinal/Robot.m
@@ -5,9 +5,52 @@
 //  Created by Иван  Балабай  on 07.12.2023.
 //
 
-#import <Foundation/Foundation.h>
+// Robot.m
+
 #import "Robot.h"
 
 @implementation Robot
+
+- (instancetype)initWithName:(NSString *)name {
+    self = [super init];
+    if (self) {
+        _name = [name copy];
+    }
+    return self;
+}
+
+- (void)saveToFile {
+    NSString *fileName = [self.name stringByAppendingString:@".txt"];
+    NSString *filePath = [self filePathForFileName:fileName];
+    NSDictionary *robotInfo = @{
+        @"coordinates": NSStringFromCGPoint(self.coordinates),
+    };
+    
+
+    [robotInfo writeToFile:filePath atomically:YES];
+}
+
+- (void)loadFromFile {
+    NSString *fileName = [self.name stringByAppendingString:@".txt"];
+    NSString *filePath = [self filePathForFileName:fileName];
+    
+
+    NSDictionary *robotInfo = [NSDictionary dictionaryWithContentsOfFile:filePath];
+    
+    if (robotInfo) {
+
+        NSString *coordinatesString = robotInfo[@"coordinates"];
+        self.coordinates = CGPointFromString(coordinatesString);
+
+    }
+}
+
+- (NSString *)filePathForFileName:(NSString *)fileName {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    
+
+    return [documentsDirectory stringByAppendingPathComponent:fileName];
+}
 
 @end
